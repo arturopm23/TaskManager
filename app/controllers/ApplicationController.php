3a4ba45cabcd24ef829b2d8d28b053aaee473e90
arturopm23 <arturopermeg23@gmail.com>
2024-06-19 11:10:10 +0200

    Estructura del TaskController

--- app/controllers/ApplicationController.php
+++ app/controllers/ApplicationController.php
@@ -6,5 +6,33 @@
  */
 class ApplicationController extends Controller 
 {
+    //Listar todas las tareas
+	public function indexAction()
+	{
+		$this->view->message = "hello from test::index";
+	}
 	
+    //Crear tarea
+	public function addAction()
+	{
+		echo "hello from test::check";
+	}
+
+    //Actualizar tarea
+    public function updateAction()
+	{
+		echo "hello from test::check";
+	}
+
+    //Borrar tarea
+    public function deleteAction()
+	{
+		echo "hello from test::check";
+	}
+
+    //Listar una tarea
+    public function infoAction()
+	{
+		echo "hello from test::check";
+	}
 }
