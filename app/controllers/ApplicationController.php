4e0b5628a451f80fad9e72087d7629f86f1a19e6
arturopm23 <arturopermeg23@gmail.com>
2024-06-25 11:34:34 +0200

    Separcion del TaskController del ApplicationController

--- app/controllers/ApplicationController.php
+++ app/controllers/ApplicationController.php
@@ -7,35 +7,4 @@
 class ApplicationController extends Controller 
 {
 
-    //Listar todas las tareas
-	public function indexAction()
-	{
-		require_once "../models/Task.class.php";
-        $tasks = Task::getAllTasks();
-        require_once "../views/homeView.php";
-	}
-	
-    //Crear tarea
-	public function addAction()
-	{
-		echo "hello from test::check";
-	}
-
-    //Actualizar tarea
-    public function updateAction()
-	{
-		echo "hello from test::check";
-	}
-
-    //Borrar tarea
-    public function deleteAction()
-	{
-		echo "hello from test::check";
-	}
-
-    //Listar una tarea
-    public function infoAction()
-	{
-		echo "hello from test::check";
-	}
 }
